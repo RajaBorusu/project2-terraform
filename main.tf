a6b96a9079aa65a2751d7c0d794b0d464a814b20
RajaBorusu <116884059+RajaBorusu@users.noreply.github.com>
2025-08-02 11:19:37 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+region= "ap-south-1"
+access_key= "AKIA6GSNG7TSQS3B7CW5"
+secret_key= "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"
+}
+
+resource "aws_instance" "one" {
+ami= "ami-0d0ad8bb301edb745"
+instance_type = "t2.medium"
+tage {
+Name="terra_instance"
+}
+}
+
+resource "aws_s3_bucket" "one" {  
+bucket = "teju-bucket"
+}
+
+resource "aws_ebs_volume" "two" {
+availability_zone = "ap-south-1a"
+size = 40
+
+  tags = {
+    Name = "rajaEBS"
+  }
+}
+
+
+
+
+
